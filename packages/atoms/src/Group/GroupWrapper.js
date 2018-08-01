fed6b142b7c8d225d5df1eefce6a271038df5d2f
Jan Hein Hoogstad <yeehaa@offcourse.io>
2018-08-01 23:48:36 +0200

    fix(general): fix gitignore

---
+++ packages/atoms/src/Group/GroupWrapper.js
@@ -0,0 +1,16 @@
+
+import system from "system-components";
+
+export default system(
+    {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        justifyContent: "flex-start",
+        flex: 1
+    },
+    "flexWrap",
+    "space",
+    "height",
+    "color"
+);
