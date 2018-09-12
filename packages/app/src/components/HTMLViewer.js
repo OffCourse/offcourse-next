3a51b127dd8b250fd89ac1c1670edd57f82ccb18
Jan Hein Hoogstad <yeehaa@offcourse.io>
2018-09-12 21:35:09 +0200

    ci(app, documentation): auto release app and documentation

--- packages/app/src/components/HTMLViewer.js
+++ packages/app/src/components/HTMLViewer.js
@@ -3,12 +3,25 @@ import { Heading, Link } from "@offcourse/atoms";
 import PropTypes from "prop-types";
 import Markdown from "markdown-to-jsx";
 import system from "system-components";
+import SyntaxHighlighter from "react-syntax-highlighter/prism-light";
+import { duotoneDark } from "react-syntax-highlighter/styles/prism";
 
 const Image = system({
   is: "img",
   maxWidth: "100%",
   maxHeight: "100%"
 });
+
+const Code = ({ children, style, ...rest }) => {
+  return (
+    <code
+      style={{ ...style, whiteSpace: "normal", overflowX: "scroll" }}
+      {...rest}
+    >
+      {children}
+    </code>
+  );
+};
 
 const Text = system({ is: "p", lineHeight: 3, fontSize: 2, py: "0.25rem" });
 
@@ -24,7 +37,16 @@ class HTMLViewer extends Component {
         options={{
           overrides: {
             h1: {
-              props: { size: "small", component: Heading }
+              props: { size: "small" },
+              component: Heading
+            },
+            pre: { component: null },
+            code: {
+              props: {
+                style: duotoneDark,
+                CodeTag: Code
+              },
+              component: SyntaxHighlighter
             },
             p: { component: Text },
             a: { component: Link },
